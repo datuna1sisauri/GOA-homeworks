ea4e5017ed8c59b031613de7d96aa097e8da8098
datuna1sisauri <datunasisauri0@gmail.com>
2024-09-30 14:32:41 +0400

    second commit

---
+++ day 1/homework/homework1.py
@@ -0,0 +1,33 @@
+name=("datuna")
+#name არის ცვლადი
+#= არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"datuna" არის ცვლადისთვის მნიშვნელობა
+
+surname=("sisauri")
+
+#print("name")
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+name=("datuna") #ეს არის str (string) ტიპის ცვლადი
+age=(15) #ეს არის int (integer) ტიპის რიცხვი
+height=177.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+studing_programing = True #true ან false
+is_footballer = False #snakecase (უბრალოდ წერის სტილი)
+
+
+#print (name + " " + surname)
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + " " + str(age)
+
+
+print(type (name))
+print(type(surname))
+print(type(age))
+print(type(height))
+print(type(studing_programing))
+
+print( name + " "  + surname + " " + str (age)+ " " +  str(height) + " " + str (studing_programing) )
