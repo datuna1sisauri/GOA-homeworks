4196ac60c7c873f177079857db477d52975fc43d
datuna1sisauri <datunasisauri0@gmail.com>
2025-02-14 14:12:47 +0400

    commit

---
+++ day38/classwork/classwork.py
@@ -0,0 +1,5 @@
+x = (4,1,43,"hello",True,False,0.1,32.3,8.1,0.001)
+def print_touple(touple1):
+    for i in range(len(touple1)):
+        print(touple1[i])
+print_touple(x)
---
+++ day38/classwork/classwork1.py
@@ -0,0 +1,6 @@
+def no_duplicates(user_list):
+    return list(set(user_list))
+    
+no_duplicates((1,3,3,3,4,1,2,4,4))
+no_duplicates((1,5,5,23,32,23,32,54,4))
+no_duplicates((4,4,5,5,6,6,7,7,8,8,))
